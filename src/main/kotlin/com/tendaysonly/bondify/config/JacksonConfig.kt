d9de431f9e5c5a4a24e5668c5e1913d29110194f
oognuyh <oognuyh@gmail.com>
2024-12-11 12:42:05 +0900

    feat: configure jackson to fix unsupported time format issues

---
+++ src/main/kotlin/com/tendaysonly/bondify/config/JacksonConfig.kt
@@ -0,0 +1,29 @@
+package com.tendaysonly.bondify.config
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
+
+/**
+ * @author oognuyh
+ */
+@Configuration
+class JacksonConfig {
+
+    @Bean
+    fun objectMapper(): ObjectMapper {
+
+        return Jackson2ObjectMapperBuilder.json()
+            .modules(
+                JavaTimeModule(),
+                KotlinModule.Builder()
+                    .build()
+            )
+            .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            .build()
+    }
+}
