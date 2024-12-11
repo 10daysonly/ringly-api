f79a5fe9fcc5bb762544b1c70d2d406347200e50
oognuyh <oognuyh@gmail.com>
2024-12-11 12:33:48 +0900

    fix: configure `context-path` to resolve CORS issues

--- src/main/kotlin/com/tendaysonly/bondify/config/SwaggerConfig.kt
+++ src/main/kotlin/com/tendaysonly/bondify/config/SwaggerConfig.kt
@@ -1,8 +1,9 @@
-package com.tendaysonly.bondify.infrastructure
+package com.tendaysonly.bondify.config
 
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.servers.Server
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -13,12 +14,14 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 class SwaggerConfig(
     @Value("\${spring.application.name}") private val title: String,
-    @Value("\${spring.application.version}") private val version: String
+    @Value("\${spring.application.version}") private val version: String,
+    @Value("\${server.servlet.context-path:/}") private val contextPath: String
 ) {
 
     @Bean
     fun openApi(): OpenAPI = OpenAPI()
         .components(Components())
+        .addServersItem(Server().url(this.contextPath))
         .info(
             Info()
                 .title(this.title)
